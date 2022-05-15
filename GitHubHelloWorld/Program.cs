22431335f4b2ce489781799c21cd21585465d4a1
SonalPrabhune <105557083+SonalPrabhune@users.noreply.github.com>
2022-05-15 13:15:15 -0400

    Update Program.cs

--- GitHubHelloWorld/Program.cs
+++ GitHubHelloWorld/Program.cs
@@ -4,6 +4,7 @@ namespace GitHubHelloWorld
 {
     internal class Program
     {
+        // Comment added by Sonal
         static void Main(string[] args) //Vera made a comment here
         {
             Console.WriteLine("Hello World!"); //test comment
